9070159e31c405b4e4cd9e773c7afd588972e43f
ZeroTrace7 <shreyashgupta999@gmail.com>
2026-03-16 09:45:31 +0530

    UC9: Validation and error handling

---
+++ InvalidBookingException.java
@@ -0,0 +1,11 @@
+/**
+ * Custom exception for invalid booking input or state.
+ *
+ * @author ZeroTrace7
+ * @version 9.0
+ */
+public class InvalidBookingException extends Exception {
+    public InvalidBookingException(String message) {
+        super(message);
+    }
+}
